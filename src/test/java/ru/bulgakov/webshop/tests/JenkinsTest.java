0d04ede92ee9ac344ac5ba62d3f4d5f90756a456
Upman <kir.seredin@gmail.com>
2026-04-15 19:03:36 +0300

    jenkins test check

--- src/test/java/ru/bulgakov/webshop/tests/JenkinsTest.java
+++ src/test/java/ru/bulgakov/webshop/tests/JenkinsTest.java
@@ -1,5 +1,6 @@
 package ru.bulgakov.webshop.tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
@@ -7,23 +8,27 @@ import org.junit.jupiter.api.Test;
 public class JenkinsTest {
     @Test
     @Tags({@Tag("UI"), @Tag("positive")})
+    @DisplayName("UI positive")
     void jenkkinsTest1() {
         System.out.println("Ui positive test");
     }
     @Test
     @Tags({@Tag("UI"), @Tag("negative")})
+    @DisplayName("UI negative")
     void jenkkinsTest2() {
         System.out.println("Ui negative test");
 
     }
     @Test
     @Tags({@Tag("API"), @Tag("positive")})
+    @DisplayName("API positive")
     void jenkkinsTest3() {
         System.out.println("API positive test");
 
     }
     @Test
     @Tags({@Tag("API"), @Tag("negative")})
+    @DisplayName("API negative")
     void jenkkinsTest4() {
         System.out.println("API negative test");
 
